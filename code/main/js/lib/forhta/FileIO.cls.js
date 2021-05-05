01ed16c5a630fe1824fb6c9305235ea3d823f974
vairy <vairydler@gmail.com>
2021-05-05 18:15:52 +0900

    FileIOをnew対応に変更

--- code/main/js/lib/forhta/FileIO.cls.js
+++ code/main/js/lib/forhta/FileIO.cls.js
@@ -1,209 +1,207 @@
 var FileIO=
 (function()
 {
-	var ret = {};
-	var fso = new ActiveXObject("Scripting.FileSystemObject");
-	
-	ret.FileExists=
-		function( filepath )
-		{
-			return fso.FileExists( filepath );
-		};
-	
-	ret.FolderExists=
-		function( filepath )
-		{
-			return fso.FolderExists( filepath );
-		};
-	
-	ret.GetFolder=
-		function( folderpath )
-		{
-			return fso.GetFolder( folderpath );
-		}
+    var ret = {};
+    var fso = new ActiveXObject("Scripting.FileSystemObject");
 
-	ret.BuildPath=
-		function( filepath, name )
-		{
-			return fso.BuildPath( filepath, name );
-		};
-		
-	ret.GetAbsolutePathName=
-		function( filepath )
-		{
-			return fso.GetAbsolutePathName( filepath );
-		};
-	
-	ret.GetParentFolderName=
-		function( filepath )
-		{
-			return fso.GetParentFolderName( filepath );
-		};
+    ret.FileExists=
+        function( filepath )
+        {
+            return fso.FileExists( filepath );
+        };
 
-	ret.GetSplitFolderRoute=
-		function( folderpath )
-		{
-			var ret = [];
-			
-			if( fso.FileExists( folderpath ) )
-			{
-				folderpath = fso.GetFile( folderpath ).ParentFolder.Path;
-			}
-			
-			if( fso.FolderExists( folderpath ) )
-			{
-				var folder = fso.GetFolder( folderpath );
-				
-				do
-				{
-					if( folder.ParentFolder )
-					{
-						ret.unshift( folder.Name );
-					}
-					else
-					{
-						ret.unshift( folder.Drive );
-					}
-				}while( null != ( folder = folder.ParentFolder ) );
-			}
-			
-			return ret;
-		};
+    ret.FolderExists=
+        function( filepath )
+        {
+            return fso.FolderExists( filepath );
+        };
 
-	ret.CollectionToArray=
-		function( collection )
-		{
-			var objEnu = new Enumerator(collection);
-			var array = [];
-			for (; !objEnu.atEnd(); objEnu.moveNext() )
-			{
-				array.push(objEnu.item());
-			}
-		    return array;
-    	};
-	
-	ret.FileReader=
-		function( filepath )
-		{
-			var ForReading = 1;
-			var ForWriting = 2;
-			var ForAppending = 8;
-			
-			try
-			{
-				this.reader = fso.OpenTextFile( filepath, ForReading, true );
-			}
-			catch(e)
-			{
-				return null;
-			}
-		};
-		
-		ret.FileReader.
-		prototype.
-		read=
-		function()
-		{
-			var ret = null;
-			if( this.reader != null )
-			{
-				try
-				{
-					ret = this.reader.readAll();
-				}
-				catch(e)
-				{
-					ret = null;
-				}
-			}
+    ret.GetFolder=
+        function( folderpath )
+        {
+            return fso.GetFolder( folderpath );
+        }
 
-			return ret;
-		};
+    ret.BuildPath=
+        function( filepath, name )
+        {
+            return fso.BuildPath( filepath, name );
+        };
 
-		ret.FileReader.
-		prototype.
-		readln=
-		function()
-		{
-			var ret = null;
-			if( this.reader != null )
-			{
-				try
-				{
-					ret = this.reader.ReadLine();
-				}
-				catch(e)
-				{
-					ret = null;
-				}
-			}
+    ret.GetAbsolutePathName=
+        function( filepath )
+        {
+            return fso.GetAbsolutePathName( filepath );
+        };
 
-			return ret;
-		};
+    ret.GetParentFolderName=
+        function( filepath )
+        {
+            return fso.GetParentFolderName( filepath );
+        };
 
-		ret.FileReader.
-		prototype.
-		close=
-		function()
-		{
-			var ret = null;
-			if( this.reader != null )
-			{
-				this.reader.Close();
-				this.reader=null;
-			}
-		};
+    ret.GetSplitFolderRoute=
+        function( folderpath )
+        {
+            var ret = [];
 
-	ret.FileWriter=
-		function( filepath )
-		{
-			var ForReading = 1;
-			var ForWriting = 2;
-			var ForAppending = 8;
-			
-			try
-			{
-				this.writer = fso.OpenTextFile( filepath, ForWriting, true );
-			}
-			catch(e)
-			{
-				return null;
-			}
-		};
+            if( fso.FileExists( folderpath ) )
+            {
+                folderpath = fso.GetFile( folderpath ).ParentFolder.Path;
+            }
 
-		ret.FileWriter.
-		prototype.
-		write=
-		function( text )
-		{
-			if( this.writer != null )
-			{
-				this.writer.write( text );
-			}
-		};
+            if( fso.FolderExists( folderpath ) )
+            {
+                var folder = fso.GetFolder( folderpath );
 
-		ret.FileWriter.
-		prototype.
-		writeln=
-		function( text )
-		{
-			if( this.writer != null )
-			{
-				this.writer.write( text + "\r\n" );
-			}
-		};
+                do
+                {
+                    if( folder.ParentFolder )
+                    {
+                        ret.unshift( folder.Name );
+                    }
+                    else
+                    {
+                        ret.unshift( folder.Drive );
+                    }
+                }while( null != ( folder = folder.ParentFolder ) );
+            }
 
-		ret.FileWriter.
-		prototype.
-		close=
-		function()
-		{
-			if( this.writer != null )
-			{
-				this.writer.Close();
-				this.writer = null;
-			}
-		};
+            return ret;
+        };
 
-	return ret;
+    ret.CollectionToArray=
+        function( collection )
+        {
+            var objEnu = new Enumerator(collection);
+            var array = [];
+            for (; !objEnu.atEnd(); objEnu.moveNext() )
+            {
+                array.push(objEnu.item());
+            }
+            return array;
+        };
+
+    ret.FileReader=
+        function( filepath, create )
+        {
+            var ForReading = 1;
+            var ForWriting = 2;
+            var ForAppending = 8;
+            var reader = null;
+            create = ( create == null ) ? true : create;
+
+            var ret={
+                read:
+                function()
+                {
+                    var ret = null;
+                    if( reader != null )
+                    {
+                        try
+                        {
+                            ret = reader.readAll();
+                        }
+                        catch(e)
+                        {
+                            ret = null;
+                        }
+                    }
+
+                    return ret;
+                },
+
+                readln:
+                function()
+                {
+                    var ret = null;
+                    if( reader != null )
+                    {
+                        try
+                        {
+                            ret = reader.ReadLine();
+                        }
+                        catch(e)
+                        {
+                            ret = null;
+                        }
+                    }
+
+                    return ret;
+                },
+
+                close:
+                function()
+                {
+                    var ret = null;
+                    if( reader != null )
+                    {
+                        reader.Close();
+                        reader=null;
+                    }
+                },
+            };
+
+            try
+            {
+                reader = fso.OpenTextFile( filepath, ForReading, create );
+                return ret;
+            }
+            catch(e)
+            {
+                return null;
+            }
+        };
+
+    ret.FileWriter=
+        function( filepath )
+        {
+            var ForReading = 1;
+            var ForWriting = 2;
+            var ForAppending = 8;
+
+            var writer;
+
+            var ret = {
+                write:
+                function( text )
+                {
+                    if( writer != null )
+                    {
+                        writer.write( text );
+                    }
+                },
+
+                writeln:
+                function( text )
+                {
+                    if( writer != null )
+                    {
+                        writer.write( text + "\r\n" );
+                    }
+                },
+
+                close:
+                function()
+                {
+                    if( writer != null )
+                    {
+                        writer.Close();
+                        writer = null;
+                    }
+                },
+            };
+
+            try
+            {
+                writer = fso.OpenTextFile( filepath, ForWriting, true );
+                return ret;
+            }
+            catch(e)
+            {
+                return null;
+            }
+        };
+
+    return ret;
 })();
